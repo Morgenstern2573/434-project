56366326e86cf61d7a4e4ebbf7c6da65db98947a
Bosun Akinyemi <azurnwa.p@gmail.com>
2024-03-24 06:13:42 +0100

    implement popup modal

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,26 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const sidebar = document.getElementById('sidebarMenu');
-    const toggleButton = document.getElementById('sidebarToggle');
+document.addEventListener("DOMContentLoaded", function () {
+  const sidebar = document.getElementById("sidebarMenu");
+  const toggleButton = document.getElementById("sidebarToggle");
 
-    toggleButton.addEventListener('click', function() {
-        sidebar.classList.toggle('collapse'); 
+  if (toggleButton) {
+    toggleButton.addEventListener("click", function () {
+      sidebar.classList.toggle("collapse");
     });
+  }
+
+  // logout modal behavior
+  const logoutModal = document.getElementById("logout-modal");
+  const logoutModalTrigger = document.getElementById("logout-modal-trigger");
+
+  if (logoutModal) {
+    logoutModalTrigger.addEventListener("click", () => {
+      logoutModal.style.display = "block";
+    });
+
+    for (let elem of document.getElementsByClassName("logout-modal-close")) {
+      elem.addEventListener("click", () => {
+        logoutModal.style.display = "none";
+      });
+    }
+  }
 });
