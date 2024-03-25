a6eaa7acced84023bd5d192c44a2d767122d77fd
Iyekolo Oluwabusayomi <iyekolobusayomi@gmail.com>
2024-03-25 18:05:26 +0100

    updated

---
+++ connection.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = "";
+$dbname = "measurement";
+
+
+$con = mysqli_connect($host, $username, $password, $dbname, 3307);
+if(!$con){
+    echo("failed to connect: ".mysqli_connect_error());
+}
+?>
